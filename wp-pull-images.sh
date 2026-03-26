c616a12afa70eeb4e468090df27a90748668df92
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2026-03-26 13:46:08 -0400

    more refactoring of another script file

--- wp-pull-images.sh
+++ wp-pull-images.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-SITE="$1";
-ENVIRONMENT="$2";
+SITE="${1:-'su-edu'}";
+ENVIRONMENT="${2:-live}";
 SERVER_USERNAME="$(terminus connection:info "$SITE"."$ENVIRONMENT" --fields=sftp_username --format=string)";
 SERVER_HOST="$(terminus connection:info "$SITE"."$ENVIRONMENT" --fields=sftp_host --format=string)";
 SERVER_PATH="code/wp-content/uploads";
 SRC="$SERVER_USERNAME@$SERVER_HOST:$SERVER_PATH";
-DEST="$HOME/Sites/$3/wp-content";
+DEST="$HOME/Sites/${3:-su}/wp-content";
+
+function fail {
+    printf '%s\n' "$1" 1>&2 ## Send message to stderr.
+    exit "${2-1}" ## Return a code specified by $2, or 1 by default.
+}
 
 function check_server_parametres() {
-  if [ -z "$1" ]
-  then
-    echo "$1 does not exist";
-    exit 1;
-  fi
+  [ -z "$1" ] || fail "$1 does not exist" 2;
 }
 
 function verify_destination() {
-  if [ ! -d "$DEST" ]
-  then
-    echo "Cannot find $DEST";
-    exit 3;
-  fi
+  [ ! -d "$DEST" ] || fail "Cannot find $DEST" 3;
 }
 
 function transfer_images() {
@@ -29,7 +26,7 @@ function transfer_images() {
   then
 	  echo "Image file transfer failed";
   else
-  	echo "Successfully transfered images";
+  	echo "Successfully transfered images from $DEST to $SRC";
   fi
 }
 
